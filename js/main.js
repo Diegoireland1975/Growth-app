cd618105d95fd0c271d2f6c5e84e86e4fb4b4a3c
Diego Raffa <diego170975@gmail.com>
2024-03-17 18:23:09 +0000

    added mobile menu

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// HAMBURGER MENU
+const hamburgerBtn = document.getElementById("hamburger-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+let isClose = true;
+
+hamburgerBtn.addEventListener("click", () => {
+  // toggle a class to show the mobile menu
+  mobileMenu.classList.toggle("open");
+  // change icon on click
+  if (isClose) {
+    hamburgerBtn.src = "images/close.png";
+  } else {
+    hamburgerBtn.src = "images/hamburger.png";
+  }
+  isClose = !isClose;
+});
